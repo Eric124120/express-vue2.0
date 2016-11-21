f491d7db70c75422c7bd4cd742c60c98f22d4db8
huangchengwen <17710053465@163.com>
2016-11-21 19:14:06 +0800

    express app set engine html

--- app.js
+++ app.js
@@ -31,9 +31,11 @@ if(isDev) {
 		}
 	}));
 	app.use(webpackHotMiddleware(compiler));
-	app.set('view engine', 'html');
 }
-
+// 设置模版
+app.set('views','./dist');
+app.engine('html', require('consolidate').handlebars);
+app.set('view engine', 'html');
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- app2.js
+++ app2.js
@@ -19,6 +19,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.set('views','./dist');
+app.engine('html', require('consolidate').handlebars);
+app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/vue', index);
